ae68be10d887755157dafcbf0c95fda3fd3cea02
Firhan <firhan7778@gmail.com>
2026-07-17 22:44:41 +0700

    ฟ

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const adminRoute = require("./routes/admin");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/admin", adminRoute);
+app.post("/api/admin/login", (req, res) => {
+    res.json({ success: true });
+});
 app.use(express.static(__dirname));
 
 const userIds = [
