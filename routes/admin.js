ca078de841ede6a8d776df02ee6c0bf177f834f5
Firhan <firhan7778@gmail.com>
2026-07-17 23:57:28 +0700

    s

--- routes/admin.js
+++ routes/admin.js
@@ -4,97 +4,75 @@ const bcrypt = require("bcrypt");
 
 const db = require("../db");
 
-
 router.post("/login", async(req,res)=>{
 
-    try{
-
-        const {password} = req.body;
+    const {password}=req.body;
 
 
-        const [rows] = await db.execute(
-            `
-            SELECT *
-            FROM admins
-            LIMIT 1
-            `
-        );
-
-         // ดึงข้อมูลจาก Database
-        const [rows] = await db.execute(
-            `
-            SELECT 
-                id,
-                username                       
-            FROM admins
-            WHERE id = ?
-            `,
-            [adminId]
-        );
+    const [adminRows] = await db.execute(
+        `
+        SELECT *
+        FROM admins
+        LIMIT 1
+        `
+    );
 
 
-
-        if(rows.length === 0){
-
-            return res.json({
-                success:false,
-                message:"ไม่พบ Admin"
-            });
-
-        }
+    const admin = adminRows[0];
 
 
-        const admin = rows[0];
+    const check = await bcrypt.compare(
+        password,
+        admin.password
+    );
 
 
-        const check = await bcrypt.compare(
-            password,
-            admin.password
-        );
-
-
-        if(!check){
-
-            return res.json({
-                success:false,
-                message:"รหัสผ่านไม่ถูกต้อง"
-            });
-
-        }
-
-
-        // สร้าง Session
-        req.session.admin = {
-
-            id: admin.id,
-            username: admin.username,
-            login:true
-
-        };
-
-
-        res.json({
-
-            success:true
-
-        });
-
-
-    }catch(err){
-
-        console.error(err);
-
-        res.status(500).json({
-
+    if(!check){
+        return res.json({
             success:false,
-            message:"Server Error"
-
+            message:"รหัสผ่านไม่ถูกต้อง"
         });
-
     }
+
+
+    req.session.admin = {
+        id:admin.id,
+        username:admin.username,
+        login:true
+    };
+
+
+    res.json({
+        success:true
+    });
 
 });
 
+
+
+router.get("/profile", async(req,res)=>{
+
+
+    const adminId = req.session.admin.id;
+
+
+    const [profileRows] = await db.execute(
+        `
+        SELECT id,username
+        FROM admins
+        WHERE id=?
+        `,
+        [adminId]
+    );
+
+
+    res.json({
+        success:true,
+        admin:profileRows[0]
+    });
+
+
+});
 
 
 // =====================
